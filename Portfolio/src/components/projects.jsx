0fb38ceeb3dd497214c6e4b2516921ea11604651
Rocco Mangone <roccom829@gmail.com>
2026-02-20 15:08:44 -0500

    Day 20 and 21
    Can't check if Geolocation project works without having an https server

--- Portfolio/src/components/projects.jsx
+++ Portfolio/src/components/projects.jsx
@@ -4,12 +4,20 @@ const Projects = () => {
     const proj1 = "VM"
     const proj2 = "Asher Dam"
     return (
-    <ul>
-        <li>This Site</li>
+    <ul onClick={handleClick}>
+        <li className='epic'>This Site</li>
         <li>{proj1}</li>
         <li>{proj2.toUpperCase()}</li>
+        <li>{temp}</li>
     </ul>
     )
 }
 
 export default Projects
+
+function handleClick (e) {
+    console.log(e);
+    this.className('epic').innerHTML('UPDATED SITE');
+}
+
+const temp = 'text';
